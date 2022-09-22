be08162839bf8cc930911b18938976e5ff3f2db4
Daniel Gomes <drg8703@gmail.com>
2022-09-22 13:45:35 -0300

    app contador de pessoas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, {useState} from "react";
+
+const App = () =>{
+  const [counter, setCounter] = useState(0)
+
+  const handleClick1 = ()  =>{
+    setCounter(counter + 1)
 }
 
-export default App;
+  const handleClick2 = () =>{
+    setCounter(counter - 1)
+}
+
+return (
+  <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '300%',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: '15%',
+  }}>
+  App contador de pessoas
+      <div style={{
+        fontSize: '120%',
+        position: 'relative',
+        top: '10vh',
+      }}>
+      {counter}
+    </div>
+    <div className="buttons">
+      <button style={{
+        fontSize: '60%',
+        position: 'relative',
+        top: '20vh',
+        marginRight: '5px',
+        backgroundColor: 'green',
+        borderRadius: '8%',
+        color: 'white',
+      }}
+        onClick={handleClick1}>Incremento</button>
+      <button style={{
+        fontSize: '60%',
+        position: 'relative',
+        top: '20vh',
+        marginLeft: '5px',
+        backgroundColor: 'red',
+        borderRadius: '8%',
+        color: 'white',
+      }}
+        onClick={handleClick2}>Decremento</button>
+    </div>
+  </div>
+)
+}
+
+export default App
